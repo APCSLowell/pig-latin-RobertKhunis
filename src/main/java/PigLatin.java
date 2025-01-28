4d76090d1480799cd726627c05853128f710b4be
RobertKhunis <rokhunis@s.sfusd.edu>
2025-01-28 11:55:12 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -1,6 +1,7 @@
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.ArrayList; // import the ArrayList class
 
 public class PigLatin {
     
